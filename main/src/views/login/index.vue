1ee7bf9c2f65e3fd08bf6f2c2dc7e17864bd3352
xiaoheiit6 <xiaoheiit@foxmail.com>
2024-03-07 20:23:08 +0800

    update and have a css error

--- main/src/views/login/index.vue
+++ main/src/views/login/index.vue
@@ -1,53 +1,48 @@
 <template>
-    <a-form
-      :model="formState"
-      name="basic"
-      :label-col="{ span: 8 }"
-      :wrapper-col="{ span: 16 }"
-      autocomplete="off"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-    >
-      <a-form-item
-        label="用户名"
-        name="username"
-        :rules="[{ required: true, message: '请输入你的用户名!' }]"
-      >
+  <div id="form" ref="main">
+    <a-form :model="formState" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off"
+      @finish="onFinish" @finishFailed="onFinishFailed" :getContainer="() => $refs.main">
+      <a-form-item label="用户名" name="username" :rules="[{ required: true, message: '请输入你的用户名!' }]">
         <a-input v-model:value="formState.username" />
       </a-form-item>
-  
-      <a-form-item
-        label="密码"
-        name="password"
-        :rules="[{ required: true, message: '请输入你的密码!' }]"
-      >
+
+      <a-form-item label="密码" name="password" :rules="[{ required: true, message: '请输入你的密码!' }]">
         <a-input-password v-model:value="formState.password" />
       </a-form-item>
-  
+
       <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
         <a-checkbox v-model:checked="formState.remember">记住我</a-checkbox>
       </a-form-item>
-  
+
       <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
         <a-button type="primary" html-type="submit">登录</a-button>
       </a-form-item>
     </a-form>
-  </template>
-  <script setup>
-  import { reactive } from 'vue';
-  const formState = reactive({
-    username: '',
-    password: '',
-    remember: true,
-  });
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-  </script>
+  </div>
+</template>
 
-<style  scoped>
+<script setup>
+import { reactive } from 'vue';
+const formState = reactive({
+  username: '',
+  password: '',
+  remember: true,
+});
+const onFinish = values => {
+  console.log('Success:', values);
+};
+const onFinishFailed = errorInfo => {
+  console.log('Failed:', errorInfo);
+};
+</script>
 
+
+
+
+<style scoped>
+:deep(.a-form-item) {
+  margin-bottom: 120px; /* 举例调整间距 */
+  margin: 1px red solid;
+  /* 其他需要调整的样式 */
+}
 </style>
