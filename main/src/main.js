3acfe2a7d72ab3bff2f97b05597c2b40f8287688
xiaoheiit6 <xiaoheiit@foxmail.com>
2024-03-15 20:57:29 +0800

    Add new methods to use vue-echarts. Use route-link to exchange data. Modified header in teacher and use flex

--- main/src/main.js
+++ main/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import router from './router'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import router from './router';
+import App from './App.vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 const pinia = createPinia()
--- main/src/views/test/echarts.vue
+++ main/src/views/test/echarts.vue
@@ -1,70 +1,43 @@
 <template>
-    <v-chart class="chart" :option="option" />
-  </template>
-  
-  <script setup>
-  import { use } from "echarts/core";
-  import { CanvasRenderer } from "echarts/renderers";
-  import { PieChart } from "echarts/charts";
-  import {
-    TitleComponent,
-    TooltipComponent,
-    LegendComponent
-  } from "echarts/components";
-  import VChart, { THEME_KEY } from "vue-echarts";
-  import { ref, provide } from "vue";
-  
-  use([
-    CanvasRenderer,
-    PieChart,
-    TitleComponent,
-    TooltipComponent,
-    LegendComponent
-  ]);
-  
-  provide(THEME_KEY, "dark");
-  
-  const option = ref({
-    title: {
-      text: "Traffic Sources",
-      left: "center"
+  <v-chart class="chart" :option="option" />
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { use } from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import { GridComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+import VChart, { THEME_KEY } from "vue-echarts";
+
+use([GridComponent, LineChart, CanvasRenderer])
+
+
+const option = computed(() => {
+  return {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 's']
     },
-    tooltip: {
-      trigger: "item",
-      formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-      orient: "vertical",
-      left: "left",
-      data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"]
+    yAxis: {
+      type: 'value'
     },
     series: [
       {
-        name: "Traffic Sources",
-        type: "pie",
-        radius: "55%",
-        center: ["50%", "60%"],
-        data: [
-          { value: 335, name: "Direct" },
-          { value: 310, name: "Email" },
-          { value: 234, name: "Ad Networks" },
-          { value: 135, name: "Video Ads" },
-          { value: 1548, name: "Search Engines" }
-        ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: "rgba(0, 0, 0, 0.5)"
-          }
-        }
+        data: [820, 932, 901, 934, 1290, 130, 160],
+        type: 'line',
+        smooth: true
       }
     ]
-  });
-  </script>
-  
-  <style scoped>
-  .chart {
-    height: 400px;
   }
-  </style>
+})
+
+
+</script>
+
+<style scoped>
+.chart {
+  height: 400px;
+  width: 500px;
+}
+</style>
