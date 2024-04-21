410e08298792b3b40013f0bc688a7b04dd10a663
xiaoheiit6 <xiaoheiit@foxmail.com>
2024-04-21 22:55:36 +0800

    introduce eslint

---
+++ main/eslint.config.mjs
@@ -0,0 +1,18 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu(
+    {
+        ignores: [],
+    },
+
+    ...compat.config({
+        rules: {
+            'curly': 'off',
+            'style/brace-style': 'off',
+            'max-statements-per-line': 'off',
+        },
+    })
+)
